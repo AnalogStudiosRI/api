8ed9541470af720cfefd4a4bf6d616a91b1590e1
Owen Buckley <owenbuckley13@gmail.com>
2022-03-29 17:23:31 -0400

    refactor back to sync invalidation call

--- src/http/post-publish/index.mjs
+++ src/http/post-publish/index.mjs
@@ -7,40 +7,42 @@ const CONFIG = {
 };
 
 export async function handler(req) {
-  try {
-    const cfClient = new AWS.CloudFront(CONFIG);
-    const entity = req.sys.contentType.sys.id;
+  const cfClient = new AWS.CloudFront(CONFIG);
+  const entity = req.sys.contentType.sys.id;
 
-    // invalidate index.html in Cloudfront
-    const params = {
-      DistributionId: 'E3MKRY7663NB8F',
-      InvalidationBatch: {
-        CallerReference: new Date().getTime(),
-        Paths: {
-          Quantity: 1,
-          Items: [
-            `/api/v2/${entity}s`
-          ]
-        }
+  // invalidate index.html in Cloudfront
+  const params = {
+    DistributionId: 'E3MKRY7663NB8F',
+    InvalidationBatch: {
+      CallerReference: new Date().getTime(),
+      Paths: {
+        Quantity: 1,
+        Items: [
+          `/api/v2/${entity}s`
+        ]
       }
-    };
+    }
+  };
 
-    await cfClient.createInvalidation(params);
-
-    return {
-      statusCode: 200,
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({ msg: 'success' })
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({ msg: e })
-    };
-  }
+  return new Promise.resolve((resolve, reject) => {
+    cfClient.createInvalidation(params).then((err) => {
+      if(err) {
+        reject({
+          statusCode: 500,
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({ msg: e })
+        })
+      } else {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({ msg: 'success' })
+        })
+      }
+    });
+  })
 }
