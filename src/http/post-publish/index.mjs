44a7a9d921779055d754fd15a77876d7d5faa653
Owen Buckley <owenbuckley13@gmail.com>
2022-03-29 20:55:33 -0400

    refactor publish lambda (#8)
    * refactor publish lambda
    
    * refactor back to sync invalidation call
    
    * cleanup and enable CORS
    
    * hail mary

--- src/http/post-publish/index.mjs
+++ src/http/post-publish/index.mjs
@@ -1,9 +1,4 @@
-// import fs from 'fs';
-// import glob from 'glob-promise';
-// import mime from 'mime-types';
-// import path from 'path';
 import * as AWS from '@aws-sdk/client-cloudfront';
-// import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 const CONFIG = {
   region: 'us-east-1',
@@ -11,21 +6,10 @@ const CONFIG = {
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
 
-// import { ListBucketsCommand } from '@aws-sdk/client-s3';
-// async function test () {
-//   try {
-//     const data = await s3Client.send(new ListBucketsCommand({}));
-//     console.log('Success', data.Buckets);
-//   } catch (err) {
-//     console.log('Error', err);
-//   }
-// }
-// test()
-
 export async function handler(req) {
-  console.debug({ req });
   const cfClient = new AWS.CloudFront(CONFIG);
-  const entity = req.sys.contentType.sys.id;
+  const body = req.body ? JSON.parse(req.body) : req; // https://stackoverflow.com/a/55354185/417806
+  const entity = body.sys.contentType.sys.id;
 
   // invalidate index.html in Cloudfront
   const params = {
@@ -61,85 +45,3 @@ export async function handler(req) {
     };
   }
 }
-
-// {
-//   "metadata": {
-//     "tags": []
-//   },
-//   "sys": {
-//     "type": "Entry",
-//     "id": "4mB0XJJDS7UWlDRulVKutx",
-//     "space": {
-//       "sys": {
-//         "type": "Link",
-//         "linkType": "Space",
-//         "id": "kpfxkjvd7pox"
-//       }
-//     },
-//     "environment": {
-//       "sys": {
-//         "id": "master",
-//         "type": "Link",
-//         "linkType": "Environment"
-//       }
-//     },
-//     "contentType": {
-//       "sys": {
-//         "type": "Link",
-//         "linkType": "ContentType",
-//         "id": "event"
-//       }
-//     },
-//     "createdBy": {
-//       "sys": {
-//         "type": "Link",
-//         "linkType": "User",
-//         "id": "2zyeBc0W6qvMF8RBEon82b"
-//       }
-//     },
-//     "updatedBy": {
-//       "sys": {
-//         "type": "Link",
-//         "linkType": "User",
-//         "id": "2zyeBc0W6qvMF8RBEon82b"
-//       }
-//     },
-//     "revision": 1,
-//     "createdAt": "2022-03-29T01:21:21.307Z",
-//     "updatedAt": "2022-03-29T01:21:21.307Z"
-//   },
-//   "fields": {
-//     "id": {
-//       "en-US": 0
-//     },
-//     "title": {
-//       "en-US": "Publish test"
-//     },
-//     "description": {
-//       "en-US": {
-//         "data": {},
-//         "content": [
-//           {
-//             "data": {},
-//             "content": [
-//               {
-//                 "data": {},
-//                 "marks": [],
-//                 "value": "dsfdsf",
-//                 "nodeType": "text"
-//               }
-//             ],
-//             "nodeType": "paragraph"
-//           }
-//         ],
-//         "nodeType": "document"
-//       }
-//     },
-//     "startTime": {
-//       "en-US": "2022-03-30T00:00"
-//     },
-//     "endTime": {
-//       "en-US": "2022-03-30T00:00"
-//     }
-//   }
-// }
