b8403da45c08f2c21ef0b384f0a93a7d12b9b66c
Owen Buckley <owen@thegreenhouse.io>
2023-11-24 19:04:14 -0500

    posts API with mysql2 local dev support

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,18 +1,14 @@
-// import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 
 export async function handler (request) {
-  // console.log({ request });
-  // const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
-  // const id = params.has('id') ? params.get('id') : null;
-  // console.log({ id });
-  // console.log('ENV', process.env.DATABASE_URL);
-  // const connection = await mysql.createConnection(process.env.DATABASE_URL);
-  // const [rows] = id
-  //   ? await connection.execute('SELECT * FROM posts WHERE id = ?', [id])
-  //   : await connection.execute('SELECT * FROM posts');
+  const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
+  const id = params.has('id') ? params.get('id') : null;
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
+  const [rows] = id
+    ? await connection.execute('SELECT * FROM posts WHERE id = ?', [id])
+    : await connection.execute('SELECT * FROM posts');
 
-  // console.log({ rows });
-  return new Response(JSON.stringify({}), {
+  return new Response(JSON.stringify(rows), {
     headers: new Headers({
       'Cache-Control': 'max-age=604800',
       'Content-Type': 'application/json',
