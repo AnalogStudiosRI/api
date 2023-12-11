302828bc9e81268bb7ffdd721331d5f971fe1e35
Owen Buckley <owen@thegreenhouse.io>
2023-12-11 16:54:59 -0500

    patch changes to protect POST publish endpoint

--- src/api/webhook/contentful.js
+++ src/api/webhook/contentful.js
@@ -11,7 +11,11 @@ export async function handler (request) {
   // TODO - double check for adapter https://stackoverflow.com/a/55354185/417806
   const cfClient = new AWS.CloudFront(CONFIG);
   const body = await request.json();
-  const entity = body.sys.contentType.sys.id;
+  const headers = request.headers;
+  const entity = body?.sys.contentType.sys.id || '';
+  const accessToken = headers.has('x-contentful_webhook_access_token')
+    ? headers.get('x-contentful_webhook_access_token')
+    : null;
 
   // invalidate index.html in Cloudfront
   const params = {
@@ -27,20 +31,26 @@ export async function handler (request) {
     }
   };
 
-  try {
-    await cfClient.createInvalidation(params);
+  if (accessToken === process.env.CONTENTFUL_WEBHOOK_ACCESS_TOKEN) {
+    try {
+      await cfClient.createInvalidation(params);
 
-    return new Response(JSON.stringify({ msg: 'success' }), { 
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    });
-  } catch (e) {
-    return new Response(JSON.stringify({ msg: e }), {
-      status: 500,
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
+      return new Response(JSON.stringify({ msg: 'success' }), { 
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      });
+    } catch (e) {
+      return new Response(JSON.stringify({ msg: e }), {
+        status: 500,
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      });
+    }
+  } else {
+    return new Response('Not Found', {
+      status: 404
     });
   }
 }
